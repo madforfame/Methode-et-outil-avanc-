0ade2acf59ad67ade79189d7d76c59cac07fb16f
costeb <benjamincoste@outlook.fr>
2018-01-26 10:49:27 +0100

    Maintenant save_load_highscore inclus util.h

--- save_load_highscore/save_load_highscore.h
+++ save_load_highscore/save_load_highscore.h
@@ -1,14 +1,4 @@
-#ifndef _SAVE_LOAD_HIGHSCORE_H_
-#define _SAVE_LOAD_HIGHSCORE_H_
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-extern int height, width, highscores;
+#include "util.h"
 
 void highscore(int high);
 void saveLoad(int *num, char **board);
-
-#endif
