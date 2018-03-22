df7de1d9340c2c283b93faa0c080402233ae35c2
costeb <34032678+costeb@users.noreply.github.com>
2018-03-22 10:13:55 +0100

    Delete niveau.h

--- niveau/niveau.h
+++
@@ -1,8 +0,0 @@
-#include "../util.h"
-#include "../tableau/tableau.h"
-
-
-void Easy(char **board, int *num);
-int Medium(char **board, char character, int *num);
-void Hard(char **board, int *num);
-
