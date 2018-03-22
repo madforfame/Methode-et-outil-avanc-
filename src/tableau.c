b3c009a8be952774b3450bd466e6c19b1a4864ff
costeb <34032678+costeb@users.noreply.github.com>
2018-03-22 10:35:00 +0100

    Update tableau.c
    Déplacement de méthodes depuis appli.c dans tableau.c

--- src/tableau.c
+++ src/tableau.c
@@ -1,4 +1,3 @@
-
 #include "tableau.h"
 
 
@@ -58,7 +57,8 @@ int checkEmpty(char **board){
 int checkCol(int num, char **board){
     int i, counter=0;
     for(i=height-1;i>=0;i--){
-        if(board[i][num-1] == VIDE){
+
+        if(num >0 && board[i][num-1] == VIDE){
             counter = 0;
             break;
         }
@@ -84,4 +84,109 @@ int checkfull(char **board)
 
     }
     return 0;
+}
+int rowNum(int num, char **board){
+    int i=0;
+    for(i=0;i<height;i++){
+        if(num == 0){
+        	return 0;
+        }
+        if(board[i][num-1]!=VIDE){
+        	break;
+        }
+    }
+    return i-1;
+}
+
+
+
+void print(char **board){
+    int i, j;
+    for(i=0; i<height; i++){
+        printf("\n+");
+
+        for(j=0; j<width; j++){
+            printf("---+");
+        }
+        
+        printf("\n|");
+   
+        for(j=0; j<width; j++){
+            printf(" %c |", board[i][j]);
+        }
+    }
+    printf("\n+");
+
+    for(i=0; i<width; i++){
+        printf("---+");
+    }
+
+    printf("\n");
+ 
+    for(i=1;i<=width;i++){
+        printf("  %d ",i);
+    }
+   
+    printf("\n\n\n");
+}
+
+
+
+
+int diagonal(char **board, char character){
+    int u, v;
+    int score1=0;
+    for(u=(height-1);u>=3;u--)
+    {
+        for(v=0;v<(width-3);v++)
+        {
+            if(board[u][v] == character && board[u-1][v+1] == character && board[u-2][v+2] == character && board[u-3][v+3] == character){
+                score1++;
+            }
+        }
+    }
+    return score1;
+}
+
+int diagonal1(char **board, char character){
+    int u, v;
+    int score1=0;
+    for(u=(height-1);u>=3;u--)
+    {
+        for(v=(width-1);v>=3;v--)
+        {
+            if(board[u][v] == character && board[u-1][v-1] == character && board[u-2][v-2] == character && board[u-3][v-3] == character){
+                score1++;
+            }
+        }
+    }
+    return score1;
+}
+int horizontalScore(char **board, char character){
+    int u, v;
+    int score1=0;
+    for(u=0;u<height;u++)
+    {
+        for(v=0;v<(width-3);v++)
+        {
+            if(board[u][v] == character && board[u][v+1] == character && board[u][v+2] == character && board[u][v+3] == character){
+                score1++;
+            }
+        }
+    }
+    return score1;
+}
+int verticalScore(char **board, char character){
+    int u, v;
+    int score1=0;
+    for(v=0;v<width;v++)
+    {
+        for(u=0;u<(height-3);u++)
+        {
+            if(board[u][v] == character && board[u+1][v] == character && board[u+2][v] == character && board[u+3][v] == character){
+                score1++;
+            }
+        }
+    }
+    return score1;
 }
