d8be1d3253c51cdd640c488ba657f5c38f7f5df2
costeb <34032678+costeb@users.noreply.github.com>
2018-03-22 10:05:54 +0100

    Update tableau.h
    Mise à jour de tableau.h par rapport aux changements de tableau.c qui contient de nouvelles méthodes

--- include/tableau.h
+++ include/tableau.h
@@ -11,5 +11,11 @@ int checkEmpty(char **board);
 int checkCol(int num, char **board);
 int checkEmpty1(char **board,int num,int numOfrow);
 int checkfull(char **board);
+int rowNum(int num, char **board);
+void print(char **board);
+int diagonal(char **board, char character);
+int diagonal1(char **board, char character);
+int horizontalScore(char **board, char character);
+int verticalScore(char **board, char character);
 
 #endif
