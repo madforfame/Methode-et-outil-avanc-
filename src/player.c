36b9aa977b2efa9cf272f53e34eb88ee6ddaf94e
hirouka <nadiabenm4@gmail.com>
2018-03-29 16:07:58 +0200

    Update player.c

--- src/player.c
+++ src/player.c
@@ -1,17 +1,17 @@
 /**
 * \file
-* \brief Méthodes permettant à l'utilisateur de jouer ???
+* \brief Méthodes permettant à l'utilisateur d'inserer un jeton 
 */
 
 #include "player.h"
 
 /**
  * \fn void player(char **board, int num, char character)
- * \brief ???
+ * \insere un jeton dans le tableau à la colonne choisie 
  *
  * \param board le plateau de jeu
- * \param num  A CONFIRMER
- * \param character A CONFIRMER
+ * \param num : numero de colonne
+ * \param character : le jeton a inserer 
  */
 void player(char **board, int num, char character){
     int i;
@@ -22,47 +22,4 @@ void player(char **board, int num, char character){
     print(board);
 }
 
-/**
- * \fn int checknum(int num, char **board ,int width)
- * \brief Récupère l'entrée de l'utilisateur jusqu'à ce qu'elle soit valide
- *
- * \param num variable de stockage de l'entier lu
- * \param board le plateau de jeu
- * \param width la largeur du plateau de jeu
- * 
- * \return l'entrée utilisateur valide
- */
-int checknum(int num, char **board ,int width){
-    while((num<-3) || (num>width))
-    {
-        print(board);
-    
-            printf("\nPlease enter a number between 1 and %d to play\n", width);
-            printf("if you want to undo, press 0\n");
-	        printf("if you want to redo, press -3, to load, press -1, to save, press -2\n");
-            num = checkentier(num);
-       
-    }
-    return num;
-}
 
-/**
- * \fn int checkentier(int num)
- * \brief Récupère l'entrée de l'utilisateur jusqu'à ce qu'elle soit valide soit un entier
- *
- * \param num variable de stockage de l'entier lu
- * 
- * \return l'entrée utilisateur valide
- */
-int checkentier(int num){
-	int err =0;
-	
-	while(err<1){
-		err=scanf("%d", &num);
-		getchar();
-		if(err<1){
-			printf("\nPlease enter a number: ");
-		}
-	}
-    return num;
-}
